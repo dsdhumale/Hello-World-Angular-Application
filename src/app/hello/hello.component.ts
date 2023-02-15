7e67396107142c4771f84efb89fc0969b30b07c3
dsdhumale <d.sdhumale@gmail.com>
2023-02-15 20:34:09 +0530

    [Devendra]Added: Ability to validate input name and if error then display the error

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -11,6 +11,7 @@ export class HelloComponent implements OnInit {
   imgUrl= "assets/BridgeLabzNewLogo.svg";
   url= "https://www.bridgelabz.com";
   userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void {
     this.message = "Hello from Bridgelabz";
@@ -19,5 +20,15 @@ export class HelloComponent implements OnInit {
   handleClick($event:any) {
     console.log("bridgelabz logo is clicked",$event);
     window.open(this.url,"_blank")
+  }
+
+  handleInputError($event: any){
+    console.log("Change event occured!!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "User Name is Invalid!!"
   }
 }
