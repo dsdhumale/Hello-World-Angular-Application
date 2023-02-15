a27e870f244c575f22480e96ffb65e275e27182f
dsdhumale <d.sdhumale@gmail.com>
2023-02-15 19:16:39 +0530

    [Devendra]Added: Ability to launch Bridgelabz site in a new tab on clicking bridgelabz logo

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -9,8 +9,14 @@ export class HelloComponent implements OnInit {
 
   message: string = 'Hello World';
   imgUrl= "assets/BridgeLabzNewLogo.svg";
+  url= "https://www.bridgelabz.com";
 
   ngOnInit(): void {
     this.message = "Hello from Bridgelabz";
+  }
+
+  handleClick(event:any) {
+    console.log("bridgelabz logo is clicked",event);
+    window.open(this.url,"_blank")
   }
 }
